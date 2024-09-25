fbbddcc1f422e7ca232d69c7438db133def02cbf
Will Fox <william.macdonald.fox@gmail.com>
2024-09-24 18:23:45 -0700

    Complete Section 1

--- built_in_methods.rb
+++ built_in_methods.rb
@@ -10,18 +10,30 @@
 
 
 "Hello World".include?("Hello")
-# the dot include method is being called on the "Hello World" string 
+# the .include? method is being called on the "Hello World" string. 
+# it is asking if the string contains the word "Hello", either true or false.
+# when we run this method, the return value is true. 
 
 "Hello World".end_with?("Hello")
+# the .end_with? method is being called on the "Hello World" string.
+# it is asking if said 'string' ends with the string "Hello", either true or false.
+# when we run this method, the return value is false.
 
 
 "Hello World".end_with?("rld")
-
+# again, the .end_with? method is being called on the "Hello World" string.
+# it is asking if said 'string' ends with the string "rld", either true or false.
+# when we run this method, the return value is true, even though it is not the full word in the string, it still ends with "rld".
 
 12.even?
-
+# the .even? method is being called on the integer 12.
+# it is asking if said integer is an even number, either true or false.
+# when we run this method, return value is true. 
 
 18.next
+# the .next method is being called on the integer 18.
+# it will return the successor of said integer, ie 18 +1.
+In this case the return value is the integer 19. 
 
 
 
