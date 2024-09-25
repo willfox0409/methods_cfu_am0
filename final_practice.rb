c5de381e5d10561dd2d537fb78227dab4bb12462
Will Fox <william.macdonald.fox@gmail.com>
2024-09-24 21:37:41 -0700

    Complete #2 & #3

--- final_practice.rb
+++ final_practice.rb
@@ -24,20 +24,18 @@ puts last_laugh
 # What is the return value of your method?
 # How many arguments did you pass your method?
 
-def greeting(name)
-  "Hey there #{name}! Welcome!"
+def greeting
+  "Hey there! Welcome!"
 end
-greet1 = greeting("Will")
-greet2 = greeting("Tim")
+
+greet1 = greeting
+greet2 = greeting
 
 puts greet1
 puts greet2
 
-# The return value of the 'greeting' method depends on the argument passed to it. 
-# The method now takes one argument (name) and incorporates it into the returned string. 
-# If a valid name is passed, the return value will be "Hey there #{name}! Welcome"
-# I pass one argument each time I call this method, such as "Will" or "Tim". 
-# I believe at this point, if the user DOESN'T pass a proper name ("Will" or "Tim"), that the method won't work correctly. 
+# The 'greeting' method returns the string "Hey there! Welcome!"
+# This method has 0 arguments. 
 
 
 # 2: Write a method named "custom_greeting" that returns a greeting WITH a specific name.
@@ -50,6 +48,23 @@ puts greet2
 # How many arguments did you pass your method?
 # What data type was your argument(s)?
 
+def custom_greeting(name)
+  "Hey there #{name}! Welcome!"
+end
+
+greet1 = custom_greeting("Will")
+greet2 = custom_greeting("Tim")
+
+puts greet1
+puts greet2
+
+# The return value of the 'custom_greeting' method depends on the argument passed to it. 
+# The method now takes one argument (name) and incorporates it into the returned string. 
+# If a valid name is passed, the return value will be "Hey there #{name}! Welcome!"
+# I pass one argument each time I call this method, such as "Will" or "Tim". 
+# The data type of this argument is a string.
+# I believe at this point, if the user DOESN'T pass a proper name ("Will" or "Tim"), that the method won't work correctly. 
+
 
 # 3: Write a method named "greet_person" that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
 
@@ -60,6 +75,20 @@ puts greet2
 # What is the return value of your method?
 # How many arguments did you pass your method?
 # What data type was your argument(s)?
+
+def greet_person(first_name, middle_name, last_name)
+  "Hey there, #{first_name} #{middle_name} #{last_name}! Welcome!"
+end
+
+greeting1 = greet_person("Will", "Macdonald", "Fox")
+greeting2 = greet_person("Carmen", "Julie", "Flood")
+
+puts greeting1
+puts greeting2
+
+# The return value of the method is a string that contains the full name in a sentence, such as "Hey there Will Macdonald Fox! Welcome!"
+# Three arguments were passed to the method each time it was called.
+# The arguments were strings
 
 
 # 4: Write a method named "square" that takes in one integer, and returns the square of that integer.
