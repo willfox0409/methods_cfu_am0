c80ca26e07874671f905a50feb90b78c749c3e1b
Will Fox <william.macdonald.fox@gmail.com>
2024-09-24 21:19:27 -0700

    Complete #1

--- final_practice.rb
+++ final_practice.rb
@@ -24,6 +24,21 @@ puts last_laugh
 # What is the return value of your method?
 # How many arguments did you pass your method?
 
+def greeting(name)
+  "Hey there #{name}! Welcome!"
+end
+greet1 = greeting("Will")
+greet2 = greeting("Tim")
+
+puts greet1
+puts greet2
+
+# The return value of the 'greeting' method depends on the argument passed to it. 
+# The method now takes one argument (name) and incorporates it into the returned string. 
+# If a valid name is passed, the return value will be "Hey there #{name}! Welcome"
+# I pass one argument each time I call this method, such as "Will" or "Tim". 
+# I believe at this point, if the user DOESN'T pass a proper name ("Will" or "Tim"), that the method won't work correctly. 
+
 
 # 2: Write a method named "custom_greeting" that returns a greeting WITH a specific name.
 
